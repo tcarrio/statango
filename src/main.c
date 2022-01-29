399ba47b35e949290b1fe98c2f1eb3c707ce0131
Tom Carrio <tom@carrio.dev>
2022-01-29 16:47:51 -0500

    feat: provide sleep functionality directly

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
+#ifndef TIMER_SECONDS
+#define TIMER_SECONDS 1
+#endif
+
+#include <unistd.h>
+
 #include "battery.c"
 #include "date.c"
 
+static const int timer_seconds = TIMER_SECONDS;
+
 int main(void) {
-    fprintf(stdout, "%s | %s", get_date(), get_battery());
+    while (1) {
+        fprintf(stdout, "%s | %s", get_date(), get_battery());
+        sleep(timer_seconds);
+    }
 
     return 0;
 }
